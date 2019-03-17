19e018b80da568618a2f2ebbea9666746dd57843
Dkreick <damian.kreick@gmail.com>
2019-03-16 23:17:59 -0300

    Adds player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Vector3 position;
+    void Start()
+    {
+        speed = 1.5f;
+        position = this.transform.position;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += speed;
+            this.transform.position = position;
+        }
+        //CLAMPS
+        if (this.transform.position.x < -65)  {
+            this.transform.position = this.transform.position;
+        }
+        if (this.transform.position.x > 65)  {
+            this.transform.position = this.transform.position;
+        }
+    }
+}
