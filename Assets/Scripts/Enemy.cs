c4949822c408f436ff0a73c88b2dad19a05cd69f
Dkreick <damian.kreick@gmail.com>
2019-03-17 23:46:14 -0300

    Adds Enemies placeholder

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    private Sprite primarySprite;
+    private Sprite secondarySprite;
+
+    void Start()
+    {
+        Debug.Log((int)Random.Range(0, 4));
+    }
+
+    void Update()
+    {
+        ChangeSprite();
+        Move();
+    }
+
+    void ChangeSprite()
+    {
+
+    }
+
+    void Move()
+    {
+
+    }
+}
