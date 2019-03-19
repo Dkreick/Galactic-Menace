8043b5fe16e76ad0a58fcc1895a3bbf5c1b8c24b
Dkreick <damian.kreick@gmail.com>
2019-03-19 00:12:38 -0300

    Adds enemies animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
 {
     private Sprite primarySprite;
     private Sprite secondarySprite;
+    public int health;
 
     void Start()
     {
@@ -14,21 +15,27 @@ public class Enemy : MonoBehaviour
         primarySprite = SpriteAtlas.dictSprites["enemy" + randomNumber + "_0"];
         secondarySprite = SpriteAtlas.dictSprites["enemy" + randomNumber + "_1"];
         GetComponent<Image>().sprite = primarySprite;
+        InvokeRepeating("ChangeSprite", 1, 1);
     }
-
-    void Update()
-    {
-        ChangeSprite();
-        Move();
-    }
-
+    
     void ChangeSprite()
     {
-
+        if (GetComponent<Image>().sprite == primarySprite)
+        {
+            GetComponent<Image>().sprite = secondarySprite;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = primarySprite;
+        }
     }
 
-    void Move()
+    void TakeDamage()
     {
-
+        health -= 1;
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
