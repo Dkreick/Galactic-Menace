cb498d48707d09ddeff4ef3ae41eb4bbf17a5d04
Dkreick <dkreick@droptek.com>
2019-06-19 11:13:44 -0300

    Implements pause script

--- Galactic-Menace/Assets/Scripts/OpenPauseMenu.cs
+++ Galactic-Menace/Assets/Scripts/OpenPauseMenu.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class OpenPauseMenu : MonoBehaviour
 {
-    public GameObject PauseMenu;
+    public GameObject pauseMenu;
+        void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown("P"))
+        if(Input.GetKeyDown (KeyCode.Escape)) 
         {
-            PauseMenu.SetActive(true);
-            print("space key was pressed");
-        }
+            if (!pauseMenu.activeInHierarchy) 
+            {
+                PauseGame();
+            }
+            if (pauseMenu.activeInHierarchy) 
+            {
+                ContinueGame();   
+            }
+        } 
+     }
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    } 
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
     }
 }
+
