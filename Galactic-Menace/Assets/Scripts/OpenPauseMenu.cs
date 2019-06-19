c188c9bada959f30bb85996e8d064d7d84bf5f0f
DKreick <damian.kreick@gmail.com>
2019-06-18 23:58:19 -0300

    Adds pause script

---
+++ Galactic-Menace/Assets/Scripts/OpenPauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenPauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown("P"))
+        {
+            PauseMenu.SetActive(true);
+            print("space key was pressed");
+        }
+    }
+}
